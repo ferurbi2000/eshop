ebbacda72b316daee51192f8b4d0bf9986e25100
Fernando Urbina <ferurbi2000@hotmail.com>
2022-03-08 13:57:12 -0600

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">eShop</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
