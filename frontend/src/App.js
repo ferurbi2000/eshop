d96097bf8ae75e28307df11edcfdbce02dfd4837
Fernando Urbina <ferurbi2000@hotmail.com>
2022-03-10 14:19:23 -0600

    Add-Rating-Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React from 'react'; // esto es nuevo
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
