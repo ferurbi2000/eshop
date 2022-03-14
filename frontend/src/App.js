ad5b7615ca1e60709020c7b51ab3916d7cf2d626
Fernando Urbina <ferurbi2000@hotmail.com>
2022-03-14 12:08:14 -0600

    Complete-Add-To-Cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,8 +26,8 @@ function App() {
                 <Link to="/cart" className="nav-link">
                   Cart
                   {cart.cartItems.length > 0 && (
-                    <Badge pill bg="danger">
-                      {cart.cartItems.length}
+                    <Badge pill bg="danger">                      
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
                     </Badge>
                   )}
                 </Link>
