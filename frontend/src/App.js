88e6b444aa79616e2899db06eea1bde4b47fc438
Fernando Urbina <ferurbi2000@hotmail.com>
2022-03-14 16:49:58 -0600

    Add-Cart-Functios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Container from 'react-bootstrap/Container';
 import { LinkContainer } from 'react-router-bootstrap';
 import Badge from 'react-bootstrap/esm/Badge';
 import { Store } from './Store';
+import CartScreen from './screens/CartScreen';
 
 function App() {
   const { state } = useContext(Store);
@@ -38,6 +39,7 @@ function App() {
         <main>
           <Container className="mt-3">
             <Routes>
+              <Route path="/cart/" element={<CartScreen />} />
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
